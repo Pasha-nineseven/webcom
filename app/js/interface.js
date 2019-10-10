36dde81b2993fd30ef0bfa583bfff4b5d01fa5e5
pasha-nineseven <pavel@nineseven.ru>
2019-10-10 18:06:19 +0300

    course start

--- app/js/interface.js
+++ app/js/interface.js
@@ -125,7 +125,10 @@ $(document).ready(function() {
 
 
 
-    
+    $('body').on('click','.js-what__info', function(e){
+        e.preventDefault();
+        alert(1);
+    });
 });
 
 
@@ -176,16 +179,17 @@ function initializeContactsMap() {
 })(jQuery);
 
 // links pages
-// $('body').append(
-// 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
-// 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
-// 	<style> \
-// 		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
-// 		#pages a { text-decoration: none; } \
-// 		#pages li { margin: 5px 0; } \
-// 	</style> \
-// 	<ol id="pages"> \
-// 		<li><a href="about.html">About</a></li> \
-// 		<li><a href="index.html">Index</a></li> \
-// 	</ol> \
-// </div>');
+$('body').append(
+	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
+		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
+	<style> \
+		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
+		#pages a { text-decoration: none; } \
+		#pages li { margin: 5px 0; } \
+	</style> \
+	<ol id="pages"> \
+		<li><a href="index.html">Index</a></li> \
+		<li><a href="spiker.html">speaker</a></li> \
+        <li><a href="course.html">course</a></li> \
+	</ol> \
+</div>');
