b509d9ebf7f0e8ddc7e6c18428996bf757a4a366
want2stayhere <want2stayhere@gmail.com>
2019-12-09 22:52:46 +0300

    media+

--- build/js/interface.js
+++ build/js/interface.js
@@ -555,6 +555,36 @@ $(document).ready(function() {
             infinite: false,
             slidesToShow: 3,
             slidesToScroll: 1,
+             responsive: [
+                {
+                  breakpoint: 768,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                  }
+                },
+                // {
+                //   breakpoint: 700,
+                //   settings: {
+                //     slidesToShow: 4,
+                //     slidesToScroll: 1,
+                //   }
+                // },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                  }
+                },
+                // {
+                //   breakpoint: 500,
+                //   settings: {
+                //     slidesToShow: 2,
+                //     slidesToScroll: 1,
+                //   }
+                // },
+            ]
         });
     };
 });
