5690dc02415d37cc4d6fad645e2273bd5ded59c0
want2stayhere <want2stayhere@gmail.com>
2020-03-22 18:56:21 +0300

    +

--- app/js/interface.js
+++ app/js/interface.js
@@ -683,19 +683,19 @@ $(document).ready(function() {
             slidesToScroll: 1,
              responsive: [
                 {
-                  breakpoint: 768,
+                  breakpoint: 1024,
                   settings: {
                     slidesToShow: 3,
                     slidesToScroll: 1,
                   }
                 },
-                // {
-                //   breakpoint: 700,
-                //   settings: {
-                //     slidesToShow: 4,
-                //     slidesToScroll: 1,
-                //   }
-                // },
+                {
+                  breakpoint: 700,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                  }
+                },
                 {
                   breakpoint: 600,
                   settings: {
